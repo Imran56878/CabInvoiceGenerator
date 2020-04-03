d661a38e05fa517469a6ba14d95fe5ac3818763c
Imran56878 <mi056878>
2020-04-03 16:45:07 +0530

    [Imran] Add CelsiusToFahrenheitEquality

--- NUnitQuantityMeasurementTest/CelsiusToFahrenheit.cs
+++ NUnitQuantityMeasurementTest/CelsiusToFahrenheit.cs
@@ -3,12 +3,22 @@ using QuantityMeasurement;
 
 namespace NUnitQuantityMeasurementTest
 {
-    public class CelsiusToFahrenheit
+    public class CelsiusToFahrenheitNUnitTest
     {
         [SetUp]
         public void Setup()
         {
 
+        }
+        /// <summary>
+        /// Checking the equality of two celsius value
+        /// </summary>
+        [Test]
+        public void Checking_Equality_Of_Celsius_Value()
+        {
+            Celsius celsius = new Celsius(1);
+            Celsius celsius1 = new Celsius(1);
+            Assert.IsTrue(celsius.Cheching_Equality_Of_Celsius_Value(celsius1));
         }
     }
 }
