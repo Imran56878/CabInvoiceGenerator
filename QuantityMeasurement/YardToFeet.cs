9d66d21d828eff72864471d92d98644df88ce489
Imran56878 <mi056878>
2020-04-03 16:00:43 +0530

    [Imran]Add Yard and Yard to inches YardToFeet classess

---
+++ QuantityMeasurement/YardToFeet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurement
+{
+    public class YardToFeet
+    {
+        int yard;
+        public YardToFeet(int _yard)
+        {
+
+            this.yard = _yard;
+        }
+        /// <summary>
+        /// it convert the yard to feet 
+        /// </summary>
+        /// <param name="yard"></param>
+        /// <returns></returns>
+        public int Converte_Yard_To_Feet(Yard yard)
+        {
+            var yardToFeet = yard.GetYard_value() * 3;
+            var feetValue = new Feet(yardToFeet);
+            return feetValue.Get_Feet_Value();
+        }
+
+        /// <summary>
+        /// This method is checking the equality of feet and yard value
+        /// </summary>
+        /// <param name="feetValue"></param>
+        /// <param name="yardValue"></param>
+        /// <returns></returns>
+        public bool Checking_Yard_And_Feet_Value_Equality(Feet feetValue, Yard yardValue)
+        {
+            var value = yardValue.GetYard_value() * 3;
+            if (feetValue.Get_Feet_Value() == value)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ QuantityMeasurement/YardToInches.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurement
+{
+  public   class YardToInches
+    {
+        /// <summary>
+        /// it convert the yard to feet 
+        /// </summary>
+        /// <param name="yard"></param>
+        /// <returns></returns>
+
+        public int Converte_Yard_To_Inches(Yard yard)
+        {
+            var yardToFeet = yard.GetYard_value() * 3*12;
+            var feetValue = new Feet(yardToFeet);
+            return feetValue.Get_Feet_Value();
+        }
+        /// <summary>
+        /// This method is checking the equality of inches and yard value
+        /// </summary>
+        /// <param name="feetValue"></param>
+        /// <param name="yardValue"></param>
+        /// <returns></returns>
+        public bool Checking_Yard_And_Feet_Value_Equality(Inches inch, Yard yardValue)
+        {
+            var value = yardValue.GetYard_value() * 3*12;
+            if (inch.getInches() == value)
+                return true;
+            return false;
+        }
+    }
+}
