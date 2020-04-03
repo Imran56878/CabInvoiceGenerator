b1b57973843cc2156aa306b1bccd368a9223156f
Imran56878 <mi056878>
2020-04-03 15:38:52 +0530

    [Imran]Add Gram with different quantityTest

--- NUnitQuantityMeasurementTest/KilogramToGramNUnitTestCases.cs
+++ NUnitQuantityMeasurementTest/KilogramToGramNUnitTestCases.cs
@@ -3,7 +3,7 @@ using QuantityMeasurement;
 
 namespace NUnitQuantityMeasurementTest
 {
-     public class KilogramToGramNUnitTestCases
+    public class KilogramToGramNUnitTestCases
     {
         [SetUp]
         public void Setup()
@@ -16,10 +16,20 @@ namespace NUnitQuantityMeasurementTest
         [Test]
         public void Checking_Equality_Of_Same_Gram_Value()
         {
-           Gram  gram1= new Gram(1);
+            Gram gram1 = new Gram(1);
             var gram2 = new Gram(1);
             Assert.IsTrue(gram1.Get_Gram_Value().Equals(gram2.Get_Gram_Value()));
 
+        }
+        /// <summary>
+        /// This Test case is checking that 1gram is equal to 2gram
+        /// </summary>
+        [Test]
+        public void Checking_Equality_Of_Different_Grams_Value()
+        {
+            var gram1 = new Gram(1);
+            var gram2 = new Gram(2);
+            Assert.IsFalse(gram1.Checking_Equality_Of_Grams(gram2));
         }
     }
 }
