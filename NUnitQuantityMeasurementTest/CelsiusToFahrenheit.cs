65802c9f11ce4b6eba684ccaac36c7b414e64373
Imran56878 <mi056878>
2020-04-03 16:53:19 +0530

    [Imran] Add checking differen celsius value in celsius to fahrenheita nd compare

--- NUnitQuantityMeasurementTest/CelsiusToFahrenheit.cs
+++ NUnitQuantityMeasurementTest/CelsiusToFahrenheit.cs
@@ -31,5 +31,26 @@ namespace NUnitQuantityMeasurementTest
             CelsiusToFahrenheit celsiusToFahrenheit = new CelsiusToFahrenheit();
             Assert.IsTrue(celsiusToFahrenheit.Checking_Equality_Of_Celsius_And_Fahrenheit_Value(celsius, fahrenheit));
         }
+        /// <summary>
+        /// Checking the equality of two celsius value
+        /// </summary>
+        [Test]
+        public void Checking_Equality_Relation_Of_Diffrent_Celsius_Value()
+        {
+            Celsius celsius = new Celsius(1);
+            Celsius celsius1 = new Celsius(2);
+            Assert.IsFalse(celsius.Cheching_Equality_Of_Celsius_Value(celsius1));
+        }
+        /// <summary>
+        /// This method is checking the value of Celsius and fahranheit
+        /// </summary>
+        [Test]
+        public void Checking_Equality_Of_Celsius_And_Fahranheit_Value()
+        {
+            Celsius celsius = new Celsius(1);
+            Fahrenheit fahranheit = new Fahrenheit(50);
+            CelsiusToFahrenheit celsiusToFahrenheit = new CelsiusToFahrenheit();
+            Assert.IsFalse(celsiusToFahrenheit.Celsius_to_fahrenheit_conversion(celsius).Equals(celsius.Get_Celsius_Value()) );
+        }
     }
 }
