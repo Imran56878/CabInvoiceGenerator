aac2840183dcce50f4f5770e8b8124f1e2dcc924
Imran56878 <mi056878>
2020-04-03 16:06:16 +0530

    [Imran]Add YardToFeetConversionTest

---
+++ NUnitQuantityMeasurementTest/YardToFeetNUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using QuantityMeasurement;
+namespace NUnitQuantityMeasurementTest
+{
+   public  class YardToFeetNUnitTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+        /// <summary>
+        /// This Method converting yard to feet value
+        /// </summary>
+        [Test]
+        public void Converting_Yard_To_Feet_Value()
+        {
+            Yard yard = new Yard(1);
+            YardToFeet yardToFeet = new YardToFeet();
+            var feetValue = yardToFeet.Converte_Yard_To_Feet(yard);
+            Assert.AreEqual(feetValue, 3);
+        }
+
+
+    }
+}
