9940d333496d3720b600471198825559684eb1c8
Imran56878 <mi056878>
2020-04-03 16:19:27 +0530

    [Imran]Add Checking_inch_And_Yard_Unequalit

--- NUnitQuantityMeasurementTest/YardToInchesNUnitTest.cs
+++ NUnitQuantityMeasurementTest/YardToInchesNUnitTest.cs
@@ -21,6 +21,17 @@ namespace NUnitQuantityMeasurementTest
             YardToInches yardToInch = new YardToInches();
             Assert.IsFalse(yardToInch. Checking_Yard_And_Inch_Value_Equality( inches, yard));
         }
+        /// <summary>
+        /// This is checking that 1 Yard and 36 Inch equality
+        /// </summary>
+        [Test]
+        public void Checking_Yard_And_Inch_Unequality()
+        {
+            Inches inches = new Inches(36);
+            Yard yard = new Yard(1);
+            YardToInches yardToInch = new YardToInches();
+            Assert.IsTrue(yardToInch.Checking_Yard_And_Inch_Value_Equality(inches, yard));
+        }
 
     }
 }
